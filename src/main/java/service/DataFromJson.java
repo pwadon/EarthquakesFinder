dc1b5419120fb02758e8c86d66c22540f6319364
Paladyn <p.wadon@gazeta.pl>
2020-04-25 17:19:30 +0200

    Taking json methods to abstract class

---
+++ src/main/java/service/DataFromJson.java
@@ -0,0 +1,46 @@
+package service;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+public abstract class DataFromJson {
+
+    /**
+     * Reads JSON from given URL
+     * @param url
+     * @return
+     * @throws IOException
+     * @throws JSONException
+     */
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream inputStream = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+
+            return json;
+        } finally {
+            inputStream.close();
+        }
+    }
+
+    /**
+     * Reads all data from given Buffer Reader
+     * @param rd
+     * @return stringBuilder.toString()
+     * @throws IOException
+     */
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            stringBuilder.append((char) cp);
+        }
+        return stringBuilder.toString();
+    }
+}
--- src/main/java/service/EarthQuakesDataFromJSON.java
+++ src/main/java/service/EarthQuakesDataFromJSON.java
@@ -10,7 +10,7 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EarthQuakesDataFromJSON {
+public class EarthQuakesDataFromJSON extends DataFromJson {
 
     /**
      * Creating list of earthquakes on earth in last 30 days from :  https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson
@@ -35,43 +35,6 @@ public class EarthQuakesDataFromJSON {
                 earthquake.setTitle(p.getString("title"));
                 earthquakes.add(earthquake);
             }
-
         return earthquakes;
     }
-
-    /**
-     * Reads JSON from given URL
-     * @param url
-     * @return
-     * @throws IOException
-     * @throws JSONException
-     */
-    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        InputStream inputStream = new URL(url).openStream();
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            JSONObject json = new JSONObject(jsonText);
-
-            return json;
-        } finally {
-            inputStream.close();
-        }
-    }
-
-    /**
-     * Reads all data from given Buffer Reader
-     * @param rd
-     * @return stringBuilder.toString()
-     * @throws IOException
-     */
-    private static String readAll(Reader rd) throws IOException {
-        StringBuilder stringBuilder = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-            stringBuilder.append((char) cp);
-        }
-        return stringBuilder.toString();
-    }
-
 }
